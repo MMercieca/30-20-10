832542c2346c6aa18704938cf6c56f8c66c0efa6
Matt Mercieca <mercieca@gmail.com>
2015-09-04 10:00:58 -0400

    Waypoint: two timers won't work.

---
+++ 30-20-10/Speed.swift
@@ -0,0 +1,77 @@
+//
+//  Speed.swift
+//  30-20-10
+//
+//  Created by Matthew Mercieca on 8/14/15.
+//  Copyright (c) 2015 Mutually Human. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum Speed: Int {
+    
+    case Jog = 1
+    case Run = 2
+    case Sprint = 3
+    case Break = 4
+    case Warmup = 5
+    
+    func description() -> String {
+        switch self {
+        case .Jog:
+            return "Jog"
+        case .Run:
+            return "Run"
+        case .Sprint:
+            return "Sprint"
+        case .Break:
+            return "Jog"
+        case .Warmup:
+            return "Warmup"
+        }
+    }
+    
+    func color() -> UIColor {
+        switch self {
+        case .Jog:
+            return UIColor.blueColor()
+        case .Run:
+            return UIColor.orangeColor()
+        case .Sprint:
+            return UIColor.redColor()
+        case .Break:
+            return UIColor.blueColor()
+        case .Warmup:
+            return UIColor.blueColor()
+        }
+    }
+    
+    func soundUrl() -> NSURL? {
+        switch self {
+        case .Jog,
+             .Warmup,
+             .Break:
+            return NSBundle.mainBundle().URLForResource("a", withExtension: "mp3")
+        case .Run:
+            return NSBundle.mainBundle().URLForResource("c", withExtension: "mp3")
+        case .Sprint:
+            return NSBundle.mainBundle().URLForResource("a2", withExtension: "mp3")
+        }
+    }
+    
+    func runFor() -> Double {
+        switch self {
+        case .Jog:
+            return 30.0
+        case .Run:
+            return 20.0
+        case .Sprint:
+            return 10.0
+        case .Break:
+            return 120.0
+        case .Warmup:
+            return 300.0
+        }
+    }
+}
--- 30-20-10/ViewController.swift
+++ 30-20-10/ViewController.swift
@@ -7,19 +7,80 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var progress: CircleProgress!
+    
+    var audioPlayer = AVAudioPlayer()
+    let calendar = NSCalendar.currentCalendar()
+    var warmup = false
+    var timer = NSTimer()
+    var intervals = [Speed]()
+    var total = 0.0
+    var timePassed = 0.0
+    var timePassedTimer = NSTimer()
+    var currentColor = UIColor.blueColor()
+    var updateInterval = 0.5
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder:aDecoder)
+        
+        AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryAmbient, error: nil)
+        AVAudioSession.sharedInstance().setActive(true, error: nil)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //TODOMPM - use one timer
+    //TODOMPM - add pausing
+    //TODOMPM - add mid app stopping
+    //TODOMPM - add setting screen (number of loops and warmup)
+    //TODOMPM - add finished state
+    
+    @IBAction func startStopPressed(sender: UIButton) {
+        if (self.warmup) {
+            intervals.append(Speed.Warmup)
+        }
+        
+        for i in 1...3 {
+            for i in 1...5
+            {
+                intervals.append(Speed.Jog)
+                intervals.append(Speed.Run)
+                intervals.append(Speed.Sprint)
+            }
+            intervals.append(Speed.Break)
+        }
+        
+        total = intervals.reduce(0, combine: { $0 + $1.runFor() })
+        
+        timerFired()
+        updateProgress()
     }
-
-
+    
+    func timerFired() {
+        if let nextInterval = intervals.first {
+            intervals.removeAtIndex(0)
+            currentColor = nextInterval.color()
+            speedLabel.text = nextInterval.description()
+            alertSpeed(nextInterval)
+            
+            timer = NSTimer(timeInterval: nextInterval.runFor(), target: self, selector: "timerFired", userInfo: nil, repeats: false)
+            //TODOMPM: this needs to work in background execution mode
+            NSRunLoop.mainRunLoop().addTimer(timer, forMode: NSRunLoopCommonModes)
+        }
+    }
+    
+    func updateProgress() {
+        timePassed += updateInterval
+        progress.pushUpdate((updateInterval*100/total, currentColor))
+        timePassedTimer = NSTimer(timeInterval: updateInterval, target: self, selector: "updateProgress", userInfo: nil, repeats: false)
+        NSRunLoop.mainRunLoop().addTimer(timePassedTimer, forMode: NSRunLoopCommonModes)
+    }
+    
+    func alertSpeed(currentSpeed:Speed) {
+        audioPlayer = AVAudioPlayer(contentsOfURL: currentSpeed.soundUrl(), error: nil)
+        audioPlayer.play()
+    }
 }
 
